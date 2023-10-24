36c32b780dc9974b81a79a41afbe80ca21fa6d2d
Victor Geadah <victorgeadah_maciii@dynamic-oit-vapornet100-10-9-51-41.princeton.edu>
2023-10-24 16:16:09 -0400

    working version for SMC on Lak model

--- models.py
+++ models.py
@@ -4,6 +4,11 @@ import scipy as sp
 def sigmoid(x, w=1.0, b=0.0, g=1.0):
     return 1/(1+np.exp(-g*(w*x + b)))
 
+def vec(x):
+    return np.array([1,x])
+def unvec(x):
+    return x[1]
+
 def sign(y) -> float:
     y = float(y)
     assert y in [0., 1.], 'y must be 0 or 1'
@@ -61,6 +66,28 @@ class QLearningModel():
         N = X.shape
         return X + self.sigma * np.random.randn(*N)
     
+    def transition_point(self, V):
+        V_L, V_R = V
+        return self.sigma * sp.special.erfinv(np.divide(V_L - V_R, V_R + V_L))
+    
+    def decision(self, m, V):
+        if self.softmax:
+            V_L, V_R = V
+            p_R = self.p_R(m)
+
+            # Compute Q values for each state
+            Qs = np.stack([np.multiply(1-p_R, V_L), np.multiply(p_R, V_R)])
+            
+            p = sp.special.softmax(Qs, axis=0)
+            if p.ndim > 1:
+                Y = np.array([np.random.binomial(1, p=_p[1]) for _p in p]).astype(int)
+            else:
+                Y = np.random.binomial(1, p=p[1])
+        else:
+            a = self.transition_point(V)
+            Y = np.array(m > a).astype(int)
+        return Y
+    
     def update_values(self, V, x, y, m):
         '''
         Learning rule update
@@ -92,24 +119,71 @@ class QLearningModel():
         else:
             return np.array(y==np.argmax(Qs, axis=0), dtype=float)
         
-    def forward(self, t, N_samples, latent_t, inputs_t, data_t):
+    def forward(self, t, N_samples, prev_latent, X_prev, Y_prev, X):
         '''
         Treating the values V and the percept m as the latents
         Return N_samples from one forward step 
             p(V_t, m_t | V_{t-1}, m_{t-1}, x_t) = p(m_t | x_t)p(V_t | V_{t-1}, m_{t-1}, x_t)
         '''
-        X_t, Y_t = inputs_t, data_t
         if t == 0:
-            V_t = self.V_init * np.ones((2, N_samples))
+            V = self.V_init * np.ones((2, N_samples))
         else:
-            V_t, m_t = latent_t
-            V_t = self.update_values(V_t, X_t, Y_t, m_t)
+            V_prev, m_prev = prev_latent
+            V = self.update_values(V_prev, X_prev, Y_prev, m_prev)
 
-        if X_t.size != N_samples:
-            m_t = np.array([self.encode(X_t) for _ in range(N_samples)])
+        if X.size != N_samples:
+            m = np.array([self.encode(X) for _ in range(N_samples)])
         else:
-            m_t = self.encode(X_t)
-        return V_t, m_t
+            m = self.encode(X)
+        return V, m
+    
+    def simulate(self, T):
+        # Generate stimulus uniformly from range
+        x_range = np.linspace(-1,1,12)
+        X = np.random.choice(x_range, size=T, replace=True)
+
+        # Encode percept and define initial values
+        m = self.encode(X)
+        V = self.V_init * np.ones(2)
+
+        # Generate decisions and values sequentially
+        Y, Vs = [], []
+        for t in range(T):
+            y = self.decision(m[t], V)
+
+            Vs.append(V)
+            Y.append(y)
+
+            V = self.update_values(V, x=X[t], y=Y[t], m=m[t])
+        return X, Y, m, Vs
+    
+class PolicyGradientGLM():
+    def __init__(self, alpha):
+        self.alpha = alpha 
+
+    def emission_likelihood(self, w, x, y=1):
+        '''p(y | w, x), default p(y=1 | w, x)'''
+        vx = vec(x)
+        LM = np.dot(w, vx)
+        p = sigmoid(sign(y) * LM)
+        return p
+
+    def decision(self, w, x):
+        p_R = self.emission_likelihood(y=1, w, x)
+        y = np.random.binomial(1, p=p_R)
+        return y
+
+    def policy_gradient_vectorized(self, w, x):
+        p_R = self.emission_likelihood(w, x, y=1)
+        p_L = 1 - p_R 
+
+        x = unvec(vx)
+        effective_reward = np.sum([
+            eps(y) * reward(x, y) for y in [0,1]
+        ])
+        return effective_reward * p_R * p_L * np.array(vx)
+
 
 if __name__=='__main__':
-    model = QLearningModel(sigma=0.3, alpha=0.5, softmax=True)
+    true_model = QLearningModel(sigma=0.3, alpha=0.5, softmax=True)
+    X, Y, m, Vs = true_model.simulate(10)
--- samplers.py
+++ samplers.py
@@ -1,5 +1,7 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from models import QLearningModel
+import scipy as sp
 
 def bootstrap_filter(N, inputs, data, model):
     '''
@@ -19,30 +21,40 @@ def bootstrap_filter(N, inputs, data, model):
         # 1. Prediction step : tilde z_t ~ p(z_t | z_{t-1})
         #   Sample proposal N particles from previous N particles
         #   {tilde z_t^i, 1/N} is an approximation to p(z_t|y_{1:t-1})
-        tilde_z_t = model.forward(t, N, z_t, X[t], Y[t])
+        if t == 0:
+            tilde_z_t = model.forward(t, N, z_t, None, Y[t], X[t])
+        else:
+            tilde_z_t = model.forward(t, N, z_t, X[t-1], Y[t], X[t])
 
         # 2. Evaluate importance weights p(y_t | xhat_t, V_t)
         #   {tilde z_t^i, tilde w^i} is an approximation to p(z_t|y_{1:t})
         if isinstance(model, QLearningModel):
             tilde_V_t, tilde_m_t = tilde_z_t
             tilde_w_t = model.emission_likelihood(y=Y[t], m=tilde_m_t, V=tilde_V_t) 
+        else:
+            raise NotImplementedError
         
         # Normalize
-        # normalized_tilde_w_t = sp.special.softmax(tilde_w_t)
-        normalized_tilde_w_t = tilde_w_t/np.sum(tilde_w_t)
-        # if np.sum(tilde_w_t) == 0.:
-        #     xhat_history.append(tilde_xhat_t)
-        #     V_history.append(tilde_V_t)
-        #     continue
+        if np.sum(tilde_w_t) == 0.:
+            choices = np.arange(N)
+        else:
+            normalized_tilde_w_t = sp.special.softmax(tilde_w_t)
+            # normalized_tilde_w_t = tilde_w_t/np.sum(tilde_w_t)
+            # if np.sum(tilde_w_t) == 0.:
+            #     xhat_history.append(tilde_xhat_t)
+            #     V_history.append(tilde_V_t)
+            #     continue
 
-        # 3. Resampling step: 
-        #   Resample with replacement N particles according the importance weights
-        choices = np.random.choice(N, size=N, p=normalized_tilde_w_t)
+            # 3. Resampling step: 
+            #   Resample with replacement N particles according the importance weights
+            choices = np.random.choice(N, size=N, p=normalized_tilde_w_t)
         if isinstance(model, QLearningModel):
             # print(tilde_m_t.shape)
             V_t = np.array([list(tilde_V_t[:,c]) for c in choices]).T
             m_t = np.array([tilde_m_t[c] for c in choices])
             z_t = V_t, m_t
+        else:
+            raise NotImplementedError
 
         # print(np.mean([emission_likelihood(y=Y[t], x_hat=_xhat, V=_V, sigma=sigma, softmax=softmax) for _xhat, _V in zip(xhat_t, V_t)]))
 
@@ -55,10 +67,42 @@ def bootstrap_filter(N, inputs, data, model):
     return z_history, loglik_running_estimate
 
 if __name__=='__main__':
-    model = QLearningModel(sigma=0.3, alpha=0.5, softmax=True)
+    true_alpha = 0.05
+    true_sigma = 0.3
+    true_model = QLearningModel(sigma=true_sigma, alpha=true_alpha, softmax=True)
+    
     T = 200
-    N = 10
+    N = 500
+    N_runs = 10
+    
+    alpha_range = np.exp(np.linspace(-5,0,20))
+    sigma_range = np.exp(np.linspace(-3,1,10))
 
-    X = np.random.randn(T)
-    Y =  np.random.randint(0, 2, size=T)
-    print(bootstrap_filter(N, inputs=X, data=Y, model=model))
+    vals = []
+    for _ in range(N_runs):
+        X, Y, _, _ = true_model.simulate(T)
+        _vals = []
+        for alpha in alpha_range:
+            model = QLearningModel(sigma=true_sigma, alpha=alpha, softmax=True)
+
+            _, log_liks = bootstrap_filter(N, inputs=X, data=Y, model=model)
+            _vals.append(log_liks)
+            print(round(alpha,3), log_liks)
+        vals.append(_vals)
+
+    vals = np.array(vals)
+
+    fig, ax = plt.subplots()
+    ax.plot(alpha_range, np.sum(vals, axis=0), c='tab:grey')
+    ax.axvline(x=true_alpha, c='k', label=r'True $\alpha$')
+    # ax.axvline(x=true_sigma, c='k', label=r'True $\sigma$')
+    # ax.plot(alpha_range, vals.T, c='tab:grey')
+    # ax.fill_between(alpha_range, mean-std, mean+std, alpha=0.3, color='tab:blue')
+    ax.legend()
+    ax.set_xlabel(r'Learning rate $\alpha$')
+    # ax.set_xlabel(r'Percept noise scale $\sigma$')
+    ax.set_ylabel('Log-likelihood')
+    # ax.set_title(f'SMC marginal likelihood estimates. $\sigma=${true_sigma:1.2f}')
+    ax.set_title(r'SMC marginal likelihood estimates. $\alpha=$'+f'{true_alpha:1.2f}')
+    ax.set_xscale('log')
+    plt.savefig('test.png')
