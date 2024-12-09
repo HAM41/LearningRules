db8aa0f1c48f9017431f392fdc0584048ee94078
Victor Geadah <vg0233@princeton.edu>
2024-12-09 14:43:28 -0500

    along w prev commit on adding Q-learning and dynamicGLMHMM, add params

--- parameters.py
+++ parameters.py
@@ -10,20 +10,28 @@ class ParamsGLMLearn(NamedTuple):
     log_sigma: Union[float, ParameterProperties]
     log_sigma_day: Union[float, ParameterProperties]
     log_alpha: Union[float, jnp.ndarray, ParameterProperties]
+    z_0: float = 0.0
+    p: float = 5.0
 
 class ParamsPsytrack(NamedTuple):
     log_sigma: float
     log_sigma_day: float
 
 class ParamsTimeVarGLMLearn(NamedTuple):
-    beta_0: float
-    log_alpha: float
-    log_sigma_0: float = -2.0
-    log_sigma: float = -2.0
-    log_sigma_day: float = -1.0
+    beta_0: float = 0.0
+    log_alpha: float = -5.0
+    log_sigma_0: float = -5.0
+    log_sigma: float = -3.0
+    log_sigma_day: float = -2.0
+    # baseline: float = 0.0
+    # log_forget: float = -4.0
+    # log_forget_day: float = -4.0
+    log_Q: jnp.ndarray = jnp.array([-4.0])
+    # r1: float = 1.0
+    # r0: float = 1.0
+    gamma: float = 1.0
+    kappa: float = 0.0
     baseline: float = 0.0
-    forget: float = 0.0
-    forget_day: float = 0.1
 
 class ParamsGLMRegLearn(NamedTuple):
     log_sigma: float
@@ -35,8 +43,6 @@ class ParamsGLMRegLearn(NamedTuple):
     gamma: float
     beta: float
     baseline: Union[float, jnp.ndarray]
-
-    
     
 class ParamsGLMHMMLearn(NamedTuple):
     logit_pi0: float
@@ -46,6 +52,35 @@ class ParamsGLMHMMLearn(NamedTuple):
     log_sigma: float
     log_sigma_day: float
 
+class ParamsGLMInterpLearn(NamedTuple):
+    log_sigma: float
+    log_sigma_day: float
+    log_alpha: float
+    Q: jnp.ndarray
+    r1: float = 1.0
+    r0: float = 1.0
+    gamma: float = 0.0
+    baseline: float = 0.0
+
+class ParamsQLearning(NamedTuple):
+    percept_log_scale: float = -2.0
+    log_sigma: float = -3.0
+    log_sigma_day: float = -2.0
+    log_alpha: float = -5.0
+    beta: float = 1.0
+
+class ParamsGLMBaseLearn(NamedTuple):
+    log_sigma: Union[float, ParameterProperties]
+    log_sigma_day: Union[float, ParameterProperties]
+    log_alpha: Union[float, jnp.ndarray, ParameterProperties]
+    baseline_weights: jnp.ndarray = jnp.zeros((5,5,))
+    log_sigma_0: float = -5.0
+    log_Q: jnp.ndarray = -5.0 * jnp.ones((5,))
+
+class ParamsDynamicGLMHMM(NamedTuple):
+    log_sigma: float
+    log_sigma_day: float = 1.0
+    alpha: float = 2.0
 
 def handle_none_params(func):
     def wrapper(self, *args, params=None, **kwargs):
@@ -74,7 +109,7 @@ def params_to_array(named_tuple) -> Tuple[jnp.ndarray, List[int]]:
     # Concatenate all arrays into a single 1D numpy array
     return jnp.concatenate(arrays), lengths
 
-def array_to_params(named_tuple, array, lengths) -> NamedTuple:
+def array_to_params(name, array, lengths) -> NamedTuple:
     # Unpack the lengths of each attribute
     L = np.cumsum(lengths)
 
@@ -84,7 +119,21 @@ def array_to_params(named_tuple, array, lengths) -> NamedTuple:
         sliced_arrays.append(array[L[i-1]:L[i]].squeeze())
     
     # Reconstruct the NamedTuple
+    if isinstance(name, str):
+        named_tuple = eval(name)
+    else:
+        named_tuple = name
     return named_tuple._make(sliced_arrays)
+
+def get_param_name(model_repr) -> str:
+    # Use regular expressions to capture any class name before the opening parenthesis
+    import re
+    match = re.match(r"(\w+)\(", model_repr)
+    if match:
+        # Prepend "Params" to the matched class name
+        return f"Params{match.group(1)}"
+    return None
+
 
 class GLMLearn_():
     def __init__(self, log_sigma: float=-1.0, alpha: float=0.0, not_trainable: list=[]) -> None:
